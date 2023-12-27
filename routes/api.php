9081890eca366095c7d013b6ffb22b97f750b83f
Afik Tahirov <afik.t38@gmail.com>
2023-12-28 01:57:09 +0400

    changed api text

--- routes/api.php
+++ routes/api.php
@@ -19,5 +19,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/valuta/{date}',[ApiController::class,"fetchAndConvertData"]);
+Route::get('/valyuta/{date}',[ApiController::class,"fetchAndConvertData"]);
 

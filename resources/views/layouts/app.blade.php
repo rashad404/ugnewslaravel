b479f2b98a183023432d61633320e9893900fdd5
Rashad M <rashad.mirzaDDC.404@gmail.com>
2024-10-06 16:54:59 -0500

    test

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,40 +11,32 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
     <!-- Styles -->
-    <link rel="stylesheet" href="{{ asset('css/app.css')  .'?'. rand(11111,99999) }}">
+    <link rel="stylesheet" href="{{ asset('css/app.css') .'?'. rand(11111,99999) }}">
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') .'?'. rand(11111,99999) }}" defer></script>
     <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
-
 </head>
-<body class="font-sans antialiased">
-    <div class="min-h-screen bg-gray-100 flex">
-
-        <!-- Main Content -->
-        <div class="flex-1 flex flex-col">
-            <!-- Top Navigation -->
-            <nav class="bg-white shadow-sm">
-                <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-                    <div class="flex justify-between h-16">
-                        <div class="flex">
-
-                            <!-- Logo -->
-                            <a href="{{ route('user.dashboard') }}" class="flex-shrink-0 flex items-center">
-                                <img class="h-8 w-auto" src="{{ asset('images/ug_news_white.svg') }}" alt="{{ config('app.name', 'Laravel') }}">
-                            </a>
-                        </div>
-                    </div>
+<body class="font-sans antialiased bg-gray-100">
+    <div class="min-h-screen flex flex-col justify-center items-center">
+        
+        <!-- Logo Header -->
+        <header class="w-full bg-white shadow-sm">
+            <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                <div class="flex justify-center h-16">
+                    <a href="{{ route('user.dashboard') }}" class="flex items-center">
+                        <img class="h-8 w-auto" src="{{ asset('images/ug_news.svg') }}" alt="{{ config('app.name', 'Laravel') }}">
+                    </a>
                 </div>
-            </nav>
+            </div>
+        </header>
 
-            <!-- Page Content -->
-            <main class="flex-1 overflow-x-hidden overflow-y-auto bg-gray-100">
-                <div class="container mx-auto px-6 py-8">
-                    @yield('content')
-                </div>
-            </main>
-        </div>
+        <!-- Page Content -->
+        <main class="flex-1 w-full">
+            <div class="container mx-auto py-8 px-4 sm:px-6 lg:px-8">
+                @yield('content')
+            </div>
+        </main>
     </div>
 </body>
 </html>

48222899a19d836f64ebf0bacf21dc0d2fa4fd11
Rashad M <rashad.mirzaDDC.404@gmail.com>
2024-10-06 02:01:21 -0500

    test

--- resources/views/user/layouts/app.blade.php
+++ resources/views/user/layouts/app.blade.php
@@ -16,7 +16,7 @@
         @stack('styles')
         
         <!-- Scripts -->
-        <script src="{{ asset('js/app.js') }}" defer></script>
+        <script src="{{ asset('js/app.js') .'?'. rand(11111,99999) }}" defer></script>
         <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
         
     </head>

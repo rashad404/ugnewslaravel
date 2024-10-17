a45dea4f43316146d67af692667882fe18a1b4b5
Rashad M <rashad.mirzaDDC.404@gmail.com>
2024-10-17 17:10:45 -0500

    new

--- resources/views/user/layouts/dropdown.blade.php
+++ resources/views/user/layouts/dropdown.blade.php
@@ -1,8 +1,8 @@
 <div class="ml-3 relative" x-data="{ open: false }">
     <div>
         <button @click="open = !open" class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition duration-150 ease-in-out">
-            @if (Auth::user()->profile_photo_url)
-                <img class="h-8 w-8 rounded-full object-cover" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" />
+            @if (Auth::user()->image)
+                <img class="h-8 w-8 rounded-full object-cover" src="{{ Storage::url(Auth::user()->image) }}" alt="{{ Auth::user()->name }}" />
             @else
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
@@ -10,7 +10,14 @@
             @endif
         </button>
     </div>
-    <div x-show="open" @click.away="open = false" class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg">
+    <div x-show="open" x-cloak @click.away="open = false" 
+         x-transition:enter="transition ease-out duration-100"
+         x-transition:enter-start="transform opacity-0 scale-95"
+         x-transition:enter-end="transform opacity-100 scale-100"
+         x-transition:leave="transition ease-in duration-75"
+         x-transition:leave-start="transform opacity-100 scale-100"
+         x-transition:leave-end="transform opacity-0 scale-95"
+         class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg">
         <div class="py-1 rounded-md bg-white shadow-xs">
             <div class="block px-4 py-2 text-xs text-gray-400">
                 {{ Auth::user()->name }}
@@ -27,3 +34,7 @@
         </div>
     </div>
 </div>
+
+<style>
+    [x-cloak] { display: none !important; }
+</style>
